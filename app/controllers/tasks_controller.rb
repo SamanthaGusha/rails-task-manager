86b74d2f99cd74f4fbf5237ba8cc3054b42da660
Samantha Gusha <samanthagusha@icloud.com>
2024-02-27 16:42:37 +0200

    rails task manager app

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -24,9 +24,15 @@ class TasksController < ApplicationController
 
   def update
     @task = Task.find(params[:id])
-    @task.update(task_params) # Will raise ActiveModel::ForbiddenAttributesError
+    @task.update(task_params)
 
     redirect_to tasks_path(@tasks)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
   end
 
   private
